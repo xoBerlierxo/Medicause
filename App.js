46356c6a114b6d7107374779ebdc1daa126aa12a
xoBerlierxo <132097817+xoBerlierxo@users.noreply.github.com>
2024-11-08 13:01:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Choosing from './screens/Choosing';
+import Blogs from './screens/Blogs';
+import Initial from './screens/Initial';
+import CameraScreen from './screens/CameraScreen';
+import Home from './screens/Home';
+import Account from './screens/Account';
+import Settings from './screens/Settings';
+import AppointmentForm from './screens/AppointmentForm';  // Import the AppointmentForm screen
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Initial">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Choosing" component={Choosing} />
+        <Stack.Screen name="Blogs" component={Blogs} />
+        <Stack.Screen name="Initial" component={Initial} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
+        <Stack.Screen name="Account" component={Account} />
+        <Stack.Screen name="Settings" component={Settings} />
+        <Stack.Screen name="Appointment" component={AppointmentForm} />
+
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
